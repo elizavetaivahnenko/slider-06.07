b87b479fa5bc440362e1b8d4c715f5a0af3cbea6
ivakhelo_e <elizavetaivahnenko3@gmail.com>
2022-07-07 00:03:04 +0300

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 let offset = 0;
 const sliderLine = document.querySelector('.image_line');
+let wol = document.querySelector('.wol');
 
 document.querySelector('.buttons_down').addEventListener('click', function(){
     offset = offset + 1070;
     if (offset > 4200) {
         offset = 0;
     }
-	sliderLine.style.top = -offset + 'px';
+    sliderLine.style.top = -offset + 'px';
+    changeBackgroundColor();
 });
 
 document.querySelector('.buttons_up').addEventListener('click', function(){
@@ -18,18 +20,14 @@ document.querySelector('.buttons_up').addEventListener('click', function(){
 	sliderLine.style.top = -offset + 'px';
 });
 
-
-
-
-// document.querySelector('.buttons_down').addEventListener('click', function changeBackgroundColor() {
-// 	let offsetWoll = 0;
-// 	offsetWoll += 1070;
-// 	if (offset > 4200) {
-// 		offset = 0;
-// 	}
-// 	let m = document.querySelector('.wol');
-// 	m.style.top = -offsetWoll + 'px';
-// });
+function changeBackgroundColor() {
+	let offsetWoll = 0;
+	offsetWoll += 1070;
+	if (offset > 4000) {
+		offsetWoll = 0;
+	}
+	wol.style.top = -offsetWoll + 'px';
+};
 
 
 
